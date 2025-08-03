38937e37dc98030f1f9b036d7a42e32c4da5dc4a
Diganta165 <mdsrdiganta@gmail.com>
2025-08-03 20:57:07 +1000

    bookDao comments

--- app/src/main/java/com/shafiur/booktracker/data/dao/BookDao.kt
+++ app/src/main/java/com/shafiur/booktracker/data/dao/BookDao.kt
@@ -17,20 +17,25 @@ interface BookDao {
     @Delete
     suspend fun delete(book: Book)
 
+//    select books based on user
     @Transaction
     @Query("SELECT * FROM books WHERE userId = :userId")
     fun getBooksWithCategories(userId: Int): Flow<List<BookWithCategory>>
 
+//    select book based on cateogry
     @Transaction
     @Query("SELECT * FROM books WHERE userId = :userId AND categoryId = :categoryId")
     fun getBooksByCategoryId(userId: Int, categoryId: Int): Flow<List<BookWithCategory>>
 
+//    all books
     @Query("SELECT * FROM books WHERE id = :bookId")
     suspend fun getBookById(bookId: Int): Book
 
+//    Update books
     @Query("UPDATE books SET status = :status WHERE id = :bookId")
     suspend fun updateStatus(bookId: Int, status: String)
 
+//    select books based on user
     @Query("SELECT * FROM books WHERE userId = :userId")
     fun getBooksByUser(userId: Int): Flow<List<Book>>
 
