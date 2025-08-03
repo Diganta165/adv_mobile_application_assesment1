606549730fab0f62dcdba97bb92845c56837d30f
Diganta165 <mdsrdiganta@gmail.com>
2025-08-03 21:13:11 +1000

    note page complete

--- app/src/main/java/com/shafiur/booktracker/data/dao/NoteDao.kt
+++ app/src/main/java/com/shafiur/booktracker/data/dao/NoteDao.kt
@@ -9,16 +9,17 @@ import kotlinx.coroutines.flow.Flow
 interface NoteDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(note: Note)
-
+//    update notes
     @Update
     suspend fun update(note: Note)
-
+//    delete notes
     @Delete
     suspend fun delete(note: Note)
-
+//    select all notes decending order
     @Query("SELECT * FROM notes WHERE bookId = :bookId ORDER BY createdAt DESC")
     fun getNotesForBook(bookId: Int): Flow<List<Note>>
 
+//    select all notes
     @Transaction
     @Query("SELECT * FROM notes")
     fun getAllNotesWithBooks(): Flow<List<NoteWithBook>>
