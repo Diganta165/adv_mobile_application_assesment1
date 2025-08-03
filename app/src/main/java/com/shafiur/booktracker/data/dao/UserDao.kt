f10ee650cf28f3b7052089ad51fb3b4251d3faac
Diganta165 <mdsrdiganta@gmail.com>
2025-08-03 21:15:12 +1000

    user page complete

--- app/src/main/java/com/shafiur/booktracker/data/dao/UserDao.kt
+++ app/src/main/java/com/shafiur/booktracker/data/dao/UserDao.kt
@@ -8,12 +8,15 @@ interface UserDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertUser(user: User): Long
 
+//    check user
     @Query("SELECT * FROM users WHERE email = :email AND password = :password")
     suspend fun login(email: String, password: String): User?
 
+//    select all the user
     @Query("SELECT * FROM users WHERE id = :id")
     suspend fun getUserById(id: Int): User?
 
+//    set new password
     @Query("UPDATE users SET password = :newPassword WHERE id = :userId")
     suspend fun updatePassword(userId: Int, newPassword: String)
 
