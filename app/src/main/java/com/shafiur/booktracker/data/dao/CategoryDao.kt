daa0657878f83ac7de7d81cf80fa1292066e1dc6
Diganta165 <mdsrdiganta@gmail.com>
2025-08-03 21:01:06 +1000

    category page comeplet

--- app/src/main/java/com/shafiur/booktracker/data/dao/CategoryDao.kt
+++ app/src/main/java/com/shafiur/booktracker/data/dao/CategoryDao.kt
@@ -10,12 +10,15 @@ interface CategoryDao {
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(category: Category)
 
+//    select categories based on user
     @Query("SELECT * FROM categories WHERE userId = :userId")
     fun getCategoriesForUser(userId: Int): Flow<List<Category>>
 
+//    Delete category
     @Delete
     suspend fun delete(category: Category)
 
+//    select all the category
     @Query("SELECT * FROM categories WHERE id = :id")
     suspend fun getCategoryById(id: Int): Category?
 }
